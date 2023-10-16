9ff84a84bc7528e403f2ad6e8288a3afcf6ef997
fedi zorai <fedi.zorai@esprit.tn>
2023-10-16 22:34:29 +0100

    all prosit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+   
+    #[Route('/author/show/{name}', name: 'show_author')]
+    public function showAuthor($name)
+    {
+        // Utilisez la variable $name dans votre logique pour afficher les détails de l'auteur
+        // Par exemple, pour afficher le nom de l'auteur :
+        
+        return $this->render('author/show.html.twig', [
+            'name' => $name,
+        ]);
+        
+    }
+
+    #[Route('/authors/list', name: 'list_authors')]
+    public function list()
+    {
+        $authors = [
+            [
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100,
+            ],
+            [
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200,
+            ],
+            [
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300,
+            ],
+        ];
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+        ]);
+    }
+}
